579542c68346d9ab78a42342faf55ae4481cd6d1
Renz Pagulayan <rcyapagulayan@gmail.com>
2019-04-20 13:34:38 +0800

    feat: hashing of the password

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,10 +1,19 @@
-import { Model, Table, Column, DataType } from "sequelize-typescript";
+import { Model, Table, Column, DataType, BeforeCreate, BeforeUpdate } from "sequelize-typescript";
+import { hash } from "bcrypt";
 
 @Table({
   tableName: "users",
   paranoid: true,
 })
 export class User extends Model<User> {
+  @BeforeCreate
+  @BeforeUpdate
+  private static async hashPassword(instance: User) {
+    if (instance.password !== instance.previous("password")) {
+      instance.password = await hash(instance.password, 12);
+    }
+  }
+  
   @Column(DataType.STRING)
   public username: string;
 
