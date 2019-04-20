fb20f9298c36ffb40fdb630804b4bcec8a26f2a9
Renz Pagulayan <rcyapagulayan@gmail.com>
2019-04-20 23:15:13 +0800

    feat: authentication

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get, UseGuards, Headers } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Controller('auth')
+export class AuthController {
+  @Get("verify")
+  @UseGuards(AuthGuard("bearer"))
+  public async verifySession(@Headers("Authentication") sessionToken: string) {
+    console.log(sessionToken);
+    return {sessionToken};
+  }
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+
+import { UserService } from "../user/user.service";
+import { User } from '../user/user.entity';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly userService: UserService,
+  ) {}
+
+  public async validateUser(token: string): Promise<User> {
+    return await this.userService.findOneByToken(token);
+  }
+}
