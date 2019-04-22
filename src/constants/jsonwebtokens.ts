1d53f214e72d42b1095dc5892578199f5ed64cc5
Renz Pagulayan <rcyapagulayan@gmail.com>
2019-04-22 20:25:56 +0800

    feat: login and jwt sessions

---
+++ src/constants/jsonwebtokens.ts
@@ -0,0 +1,2 @@
+
+export const JWT_PRIVATE_KEY = process.env.JWT_PRIVATE_KEY || "supercalifragilisticexpialidocious";
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,11 +1,35 @@
-import { Controller, Get, UseGuards, Headers } from "@nestjs/common";
+import { Controller, Get, UseGuards, Headers, Body, Post, UnprocessableEntityException } from "@nestjs/common";
+import { sign } from "jsonwebtoken";
 import { AuthGuard } from "@nestjs/passport";
+import { compare } from "bcrypt";
+import { Op } from "sequelize";
+
+import { JWT_PRIVATE_KEY } from "../../constants/jsonwebtokens";
+import { CredentialsDto } from "./dto/credentials.dto";
+import { UserService } from "../user/user.service";
 
 @Controller("auth")
 export class AuthController {
+  constructor(
+    private readonly userService: UserService,
+  ) {}
+
   @Get("verify")
   @UseGuards(AuthGuard("bearer"))
   public async verifySession(@Headers("Authorization") sessionToken: string) {
     return {sessionToken};
+  }
+
+  @Post()
+  public async login(@Body() credentialDto: CredentialsDto) {
+    const user = await this.userService.findOne({where: {username: {[Op.eq]: credentialDto.username}}});
+    if (!user || !await compare(credentialDto.password, user.password)) {
+      return new UnprocessableEntityException("Incorrect username/password");
+    }
+
+    user.token = sign({username: user.username, id: user.id}, JWT_PRIVATE_KEY);
+    user.save();
+
+    return {token: user.token};
   }
 }
