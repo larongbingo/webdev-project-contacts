b8653aa2e9566bb1c3a3b6af1827f950af54be27
Renz Pagulayan <rcyapagulayan@gmail.com>
2019-04-23 07:59:40 +0800

    fix: moved credential check to AuthService

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,15 +1,26 @@
 import { Injectable } from "@nestjs/common";
+import { Op } from "sequelize";
 
 import { UserService } from "../user/user.service";
 import { User } from "../user/user.entity";
+import { BcryptRepositoryService } from "../repositories/bcrypt-repository.service";
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly userService: UserService,
+    private readonly bcryptRepositoryService: BcryptRepositoryService,
   ) {}
 
   public async validateUser(token: string): Promise<User> {
     return await this.userService.findOneByToken(token);
+  }
+
+  public async validateCredentials(username: string, password: string): Promise<User | null> {
+    const user = await this.userService.findOne({where: {username: {[Op.eq]: username}}});
+    if (!user || !await this.bcryptRepositoryService.compare(password, password)) {
+      return null;
+    }
+    return user;
   }
 }
