ca04db1ac4153f4957946de9f80ff226895fe24a
Renz Pagulayan <rcyapagulayan@gmail.com>
2019-04-26 18:21:48 +0800

    chore: removed unnecessary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { DatabaseModule } from "./modules/database/database.module";
 import { UserModule } from "./modules/user/user.module";
 import { AuthModule } from "./modules/auth/auth.module";
 
 @Module({
   imports: [DatabaseModule, UserModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
