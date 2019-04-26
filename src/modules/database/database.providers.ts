4213b52ae09766068fd306878d814404ef993f98
Renz Pagulayan <rcyapagulayan@gmail.com>
2019-04-26 22:41:24 +0800

    revert: fix: error timed out connecting to mysql

--- src/modules/database/database.providers.ts
+++ src/modules/database/database.providers.ts
@@ -14,6 +14,7 @@ export const databaseProviders: Provider[] = [
         username: process.env.MYSQL_USERNAME || "root",
         password: process.env.MYSQL_PASSWORD || "root",
         host: process.env.MYSQL_HOST || "127.0.0.1",
+        port: Number(process.env.MYSQL_PORT) || 3306,
         dialect: "mysql",
       });
 
