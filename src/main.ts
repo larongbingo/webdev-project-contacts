3ede179d84a91a0a4df8a68e29314a3f4355c874
Renz Pagulayan <rcyapagulayan@gmail.com>
2019-04-27 00:00:29 +0800

    feat: use CORS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {logger: console});
+  app.enableCors();
   app.use(compression());
   await app.listen(process.env.PORT || 8080, process.env.HOST || "127.0.0.1");
 }
