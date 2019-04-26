4e16d3675c09a4fbe16b5d7ca49377e3f5a67a89
Renz Pagulayan <rcyapagulayan@gmail.com>
2019-04-26 23:58:09 +0800

    feat: logging

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import * as compression from "compression";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {logger: console});
   app.use(compression());
   await app.listen(process.env.PORT || 8080, process.env.HOST || "127.0.0.1");
 }
