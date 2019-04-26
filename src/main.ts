a6b4f7daf2d4b8e28600331f5448a4702849a570
Renz Pagulayan <rcyapagulayan@gmail.com>
2019-04-26 23:11:52 +0800

    feat: env for port and host

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(compression());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 8080, process.env.HOST || "127.0.0.1");
 }
 bootstrap();
