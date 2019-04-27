a3abbea206f9424bdc0adea64c7ded9065c1cc9a
Renz Pagulayan <rcyapagulayan@gmail.com>
2019-04-27 14:25:35 +0800

    feat: close connections on shutdown

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableShutdownHooks();
   app.enableCors();
   app.use(logger("combined"));
   app.use(compression());
--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -1,9 +1,14 @@
-import { Module } from "@nestjs/common";
+import { Module, OnApplicationShutdown } from "@nestjs/common";
 
+import { databaseConnection } from "./database.connection";
 import { databaseProviders } from "./database.providers";
 
 @Module({
   providers: [...databaseProviders],
   exports: [...databaseProviders],
 })
-export class DatabaseModule {}
+export class DatabaseModule implements OnApplicationShutdown {
+  public async onApplicationShutdown(signal: string) {
+    await databaseConnection.close();
+  }
+}
