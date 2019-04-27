1fcea2042b02799fbbedde2ce940144d09c2288e
Renz Pagulayan <rcyapagulayan@gmail.com>
2019-04-27 12:10:20 +0800

    feat: use combined on morgan

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.use(logger("common"));
+  app.use(logger("combined"));
   app.use(compression());
   await app.listen(process.env.PORT || 8080, process.env.HOST || "127.0.0.1");
 }
