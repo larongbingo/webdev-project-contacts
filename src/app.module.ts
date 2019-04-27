6697f47249a198251ca6a2910b65cdb1ba7f652c
Renz Pagulayan <rcyapagulayan@gmail.com>
2019-04-27 20:22:06 +0800

    feat(app-module): imports message-module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
+
+import { MessageModule } from "./modules/messages/message.module";
 import { DatabaseModule } from "./modules/database/database.module";
 import { UserModule } from "./modules/user/user.module";
 import { AuthModule } from "./modules/auth/auth.module";
 
 @Module({
-  imports: [DatabaseModule, UserModule, AuthModule],
+  imports: [DatabaseModule, UserModule, AuthModule, MessageModule],
   controllers: [],
   providers: [],
 })
