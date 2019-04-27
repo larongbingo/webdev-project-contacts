892667141b9e9f9ed26909412c1cb1487a203706
Renz Pagulayan <rcyapagulayan@gmail.com>
2019-04-27 12:53:11 +0800

    feat: check if username is unique

--- src/modules/user/user.entity.ts
+++ src/modules/user/user.entity.ts
@@ -1,11 +1,22 @@
 import { Model, Table, Column, DataType, BeforeCreate, BeforeUpdate } from "sequelize-typescript";
+import { BadRequestException } from "@nestjs/common";
 import { hash } from "bcrypt";
-
+import { Op } from "sequelize";
 @Table({
   tableName: "users",
   paranoid: true,
 })
 export class User extends Model<User> {
+  @BeforeCreate
+  @BeforeUpdate
+  private static async isUsernameUnique(instance: User) {
+    if (instance.username !== instance.previous("username")) {
+      if (!await this.findOne({where: {username: {[Op.eq]: instance.username}}})) {
+        throw new BadRequestException("Username is already taken");
+      }
+    }
+  }
+
   @BeforeCreate
   @BeforeUpdate
   private static async hashPassword(instance: User) {
