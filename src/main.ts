01b578fbd4336535360de7a1aeac89dd1f6e3fe1
Renz Pagulayan <rcyapagulayan@gmail.com>
2019-04-27 12:03:58 +0800

    feat: log requests

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from "@nestjs/core";
 import * as compression from "compression";
+import * as logger from "morgan";
 
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use(logger("common"));
   app.use(compression());
   await app.listen(process.env.PORT || 8080, process.env.HOST || "127.0.0.1");
 }
