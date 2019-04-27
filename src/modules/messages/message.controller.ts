f2aacc5db36ad7905a13227b5849a3bae3627c33
Renz Pagulayan <rcyapagulayan@gmail.com>
2019-04-27 19:01:59 +0800

    feat: message details route and reduce data

--- src/modules/messages/message.controller.ts
+++ src/modules/messages/message.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Body, Post, Get, UseGuards, Query } from "@nestjs/common";
+import { Controller, Body, Post, Get, UseGuards, Query, Param } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 import { Op } from "sequelize";
 
@@ -21,12 +21,23 @@ export class MessageController {
   @Get()
   @UseGuards(AuthGuard("bearer"))
   public async findAllMessages(@Query() findMessageDto: FindMessageDto) {
-    return await this.MESSAGE_SERVICE.findAll({
+    const messages = await this.MESSAGE_SERVICE.findAll({
       where: {
         email: (findMessageDto.email) ? { [Op.like]: `%${findMessageDto.email}%` } : null,
         title: (findMessageDto.title) ? { [Op.like]: `%${findMessageDto.title}%` } : null,
         message: (findMessageDto.message) ? { [Op.like]: `%${findMessageDto.message}%` } : null,
       },
     });
+    // tslint:disable-next-line: prefer-for-of
+    for (let i = 0; i < messages.length; i++) {
+      messages[i].message = messages[i].message.substring(0, 30);
+    }
+    return messages;
+  }
+
+  @Get(":messageId")
+  @UseGuards(AuthGuard("bearer"))
+  public async getMessageDetails(@Param("messageId") messageId: string) {
+    return await this.MESSAGE_SERVICE.findOne({where: {id: {[Op.eq]: messageId}}});
   }
 }
